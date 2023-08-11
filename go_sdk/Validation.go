54e6e2df63c7181c953a196ec896973220562118
BGDTC <81570834+bgdtc@users.noreply.github.com>
2023-08-11 12:59:50 +0200

    up

--- go_sdk/Validation.go
+++ go_sdk/Validation.go
@@ -1,9 +1,9 @@
 package go_sdk
 
 import (
-	"fmt"
-	"time"
 	"encoding/json"
+	"time"
+
 	"github.com/go-playground/validator/v10"
 )
 
@@ -18,35 +18,35 @@ type Payment struct {
 }
 
 type Transaction struct {
-	ReferenceID       string     `validate:"required"`
-	Amount            float64    `validate:"required,gt=0"`
-	CustomerID        string     `validate:"required"`
-	TransactionDate   time.Time  `validate:"required"`
-	StoreName         string     `validate:"required"`
-	BillingDescriptor string     `validate:"required"`
-	Siret             string     `validate:"omitempty,number"`
-	Payment           Payment    `validate:"required"`
-	Currency          string     `validate:"required,oneof=EUR USD"`
-	MerchantName      string     `validate:""`
+	ReferenceID       string    `validate:"required"`
+	Amount            float64   `validate:"required,gt=0"`
+	CustomerID        string    `validate:"required"`
+	TransactionDate   time.Time `validate:"required"`
+	StoreName         string    `validate:"required"`
+	BillingDescriptor string    `validate:"required"`
+	Siret             string    `validate:"omitempty,number"`
+	Payment           Payment   `validate:"required"`
+	Currency          string    `validate:"required,oneof=EUR USD"`
+	MerchantName      string    `validate:""`
 }
 
 type ReceiptPayload struct {
-	ReferenceID    string     `validate:"required"`
-	Amount         float64    `validate:"required"`
-	TotalTaxAmount float64    `validate:""`
-	Currency       string     `validate:"required,oneof=EUR USD"`
-	Date           string     `validate:"required,datetime"`
-	Covers         int        `validate:""`
-	Table          string     `validate:""`
-	Invoice        int        `validate:""`
-	TotalDiscount  float64    `validate:""`
-	Mode           string     `validate:""`
-	PartnerName    string     `validate:"required"`
-	Merchant       Merchant   `validate:"required"`
-	Store          Store      `validate:"required"`
-	Taxes          []Tax      `validate:""`
-	Items          []Item     `validate:"required"`
-	Payments       []Payment  `validate:"required"`
+	ReferenceID    string    `validate:"required"`
+	Amount         float64   `validate:"required"`
+	TotalTaxAmount float64   `validate:""`
+	Currency       string    `validate:"required,oneof=EUR USD"`
+	Date           string    `validate:"required,datetime"`
+	Covers         int       `validate:""`
+	Table          string    `validate:""`
+	Invoice        int       `validate:""`
+	TotalDiscount  float64   `validate:""`
+	Mode           string    `validate:""`
+	PartnerName    string    `validate:"required"`
+	Merchant       Merchant  `validate:"required"`
+	Store          Store     `validate:"required"`
+	Taxes          []Tax     `validate:""`
+	Items          []Item    `validate:"required"`
+	Payments       []Payment `validate:"required"`
 }
 
 type Merchant struct {
@@ -56,13 +56,13 @@ type Merchant struct {
 }
 
 type Store struct {
-	StoreName         string   `validate:"required"`
-	ReferenceID       string   `validate:"required"`
-	BillingDescriptor string   `validate:"required"`
-	Siret             string   `validate:"required"`
-	CodeApe           string   `validate:""`
-	TvaIntra          string   `validate:""`
-	Address           Address  `validate:"required"`
+	StoreName         string  `validate:"required"`
+	ReferenceID       string  `validate:"required"`
+	BillingDescriptor string  `validate:"required"`
+	Siret             string  `validate:"required"`
+	CodeApe           string  `validate:""`
+	TvaIntra          string  `validate:""`
+	Address           Address `validate:"required"`
 }
 
 type Address struct {
@@ -81,27 +81,27 @@ type Tax struct {
 }
 
 type Item struct {
-	ReferenceID  string `validate:""`
-	Name         string `validate:"required"`
-	Description  string `validate:""`
-	Type         string `validate:""`
-	Quantity     int    `validate:"required"`
-	Price        float64 `validate:"required"`
-	Discount     float64 `validate:""`
-	TotalAmount  float64 `validate:""`
-	Tax          Tax     `validate:"required"`
-	Subitems     []Subitem `validate:""`
+	ReferenceID string    `validate:""`
+	Name        string    `validate:"required"`
+	Description string    `validate:""`
+	Type        string    `validate:""`
+	Quantity    int       `validate:"required"`
+	Price       float64   `validate:"required"`
+	Discount    float64   `validate:""`
+	TotalAmount float64   `validate:""`
+	Tax         Tax       `validate:"required"`
+	Subitems    []Subitem `validate:""`
 }
 
 type Subitem struct {
-	ReferenceID  string `validate:""`
-	Name         string `validate:"required"`
-	Description  string `validate:""`
-	Quantity     int    `validate:""`
-	Price        float64 `validate:""`
-	Discount     float64 `validate:""`
-	TotalAmount  float64 `validate:""`
-	Tax          Tax     `validate:"required"`
+	ReferenceID string  `validate:""`
+	Name        string  `validate:"required"`
+	Description string  `validate:""`
+	Quantity    int     `validate:""`
+	Price       float64 `validate:""`
+	Discount    float64 `validate:""`
+	TotalAmount float64 `validate:""`
+	Tax         Tax     `validate:"required"`
 }
 
 func ValidateTransactionPayload(data []byte) bool {
