c8f478578204cf29d78b5c914178f1f94ad9996f
flozonn <florian.breton1@icloud.com>
2023-08-14 16:59:51 +0200

    Create SendReceipt.go

---
+++ go_sdk/SendReceipt.go
@@ -0,0 +1,12 @@
+package go_sdk
+import (
+	"encoding/json"
+)
+
+func SendReceipt(env string, receiptData []byte, hmacKey string) (string, bool) {
+	compactReceiptData, err := json.Marshal(json.RawMessage(receiptData))
+    if err != nil {
+        return "", false
+    }
+	return SendDataWithHMAC(env, "receipt", compactReceiptData, hmacKey, ValidateReceiptPayload)
+}
